001ca3a0a08e9999f9f51b6d162ba468e645ac07
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-04-25 20:21:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+var userInput = prompt();
+
+let arr = userInput.split('');
+
+function nonRepChar(arr) {
+	
+	for(let i = 0; i < arr.length; i++){
+		let ct = 0;
+		for(let j = i+1; j < arr.length; j++){
+			if(arr[i] == arr[j]){
+				ct++;
+			}
+		}
+
+		if(ct == 0){
+			return arr[i];
+		}
+	}
+}
+
+alert(nonRepChar(arr));
+
+
+
+
+// alert(userInput);
